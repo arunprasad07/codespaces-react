5a5f97df3aa29262c3e9a776d943c066f518e69b
arunprasad07 <arunprasad.azhagesan@gmail.com>
2023-07-23 10:24:36 +0000

    Toggle cart redux state implementation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import './App.css';
+import { useSelector } from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
 
 function App() {
+  const showCart = useSelector(state => state.ui.cartIsVisible);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <Layout>
+      {showCart && <Cart />}
+      <Products />
+    </Layout>
   );
 }
 
